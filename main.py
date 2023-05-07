3ddfc814fddf76409d1c89789758f6ccc9f0be0e
macsumch <114281696+macsumch@users.noreply.github.com>
2023-05-07 17:58:55 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+class GameSprite(sprite.Sprite):
+    # конструктор класу
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        # викликаємо конструктор класу (Sprite):
+        sprite.Sprite.__init__(self)
+ 
+        # кожен спрайт повинен зберігати властивість image - зображення
+        self.image = transform.scale(
+            image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+ 
+        # кожен спрайт повинен зберігати властивість rect - прямокутник, в який він вписаний
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+    # метод, що малює героя у вікні
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+ 
+# клас головного гравця
+class Player(GameSprite):
+    # метод для керування спрайтом стрілками клавіатури
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+ 
+      def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_W] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_S] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed 
+    # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
